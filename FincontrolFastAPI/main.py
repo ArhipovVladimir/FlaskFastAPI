4112744757f48c0d33d3fee6d493039229cab3ae
ArhipovVS <arhipof05@yndex.ru>
2023-08-15 10:45:19 +0700

    FastAPI Seminar 6

--- FincontrolFastAPI/main.py
+++ FincontrolFastAPI/main.py
@@ -1,7 +1,6 @@
-import datetime
-
 import databases
 import sqlalchemy
+from datetime import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from typing import List
@@ -14,113 +13,113 @@ database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
 # Реестр
-reestr = sqlalchemy.Table(
-    "reests",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("code", sqlalchemy.String(5)),
-    sqlalchemy.Column("operation_id", sqlalchemy.ForeignKey('operation.id'), nullable=False),
-    sqlalchemy.Column("worker", sqlalchemy.ForeignKey('worker.id'), nullable=False),
-    sqlalchemy.Column("control_action", sqlalchemy.ForeignKey('control_action.id'), nullable=False),
-    sqlalchemy.Column("method", sqlalchemy.ForeignKey('method.id'), nullable=False),
-
-)
+# reestr = sqlalchemy.Table(
+#     "reestr",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("code", sqlalchemy.String(5)),
+#     sqlalchemy.Column("operation_id", sqlalchemy.ForeignKey('operation.id'), nullable=False),
+#     sqlalchemy.Column("worker", sqlalchemy.ForeignKey('worker.id'), nullable=False),
+#     sqlalchemy.Column("control_action", sqlalchemy.ForeignKey('control_action.id'), nullable=False),
+#     sqlalchemy.Column("method", sqlalchemy.ForeignKey('method.id'), nullable=False),
+#
+# )
 
 
 # Процессы
-process = sqlalchemy.Table(
-    "process",
+prs = sqlalchemy.Table(
+    "prs",
     metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("id", sqlalchemy.Integer,  primary_key=True),
     sqlalchemy.Column("name", sqlalchemy.String(128)),
 
 )
 
 
-# Операции
-operation = sqlalchemy.Table(
-    "operation",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("name", sqlalchemy.String(128)),
-    sqlalchemy.Column("process_id", sqlalchemy.ForeignKey('process.id'), nullable=False),
-
-)
-
-
-# должность работника
-employ_position = sqlalchemy.Table(
-    "employ_position",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("name", sqlalchemy.String(128)),
-
-)
-
-# работник
-worker = sqlalchemy.Table(
-    "worker",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("name", sqlalchemy.String(128)),
-    sqlalchemy.Column("surname", sqlalchemy.String(128)),
-    sqlalchemy.Column("employ_position", sqlalchemy.ForeignKey('employ_position.id'), nullable=False),
-)
-
-
-# конрольное действие
-control_action = sqlalchemy.Table(
-    "control_action",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("name", sqlalchemy.String(128)),
-
-)
-
-# метод котроля
-method = sqlalchemy.Table(
-    "method",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("name", sqlalchemy.String(128)),
-
-)
-# Справка о нарушении
-certificate_of_violations = sqlalchemy.Table(
-    "certificate_of_violations",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("date", sqlalchemy.Date, default=datetime.datetime.utcnow),
-    sqlalchemy.Column("worker", sqlalchemy.ForeignKey('worker.id'), nullable=False),
-
-)
-
-# Строка в справке о нарушениях
-violation = sqlalchemy.Table(
-    "violation",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("certificate_of_violations", sqlalchemy.ForeignKey('certificate_of_violations.id'), nullable=False),
-    sqlalchemy.Column("core_reestr", sqlalchemy.ForeignKey('reestr.id'), nullable=False),
-    sqlalchemy.Column("title", sqlalchemy.String(128)),
-    sqlalchemy.Column("employ_position", sqlalchemy.ForeignKey('employ_position.id'), nullable=False),
-    sqlalchemy.Column("worker", sqlalchemy.ForeignKey('worker.id'), nullable=False),
-    sqlalchemy.Column("amount", sqlalchemy.Float),
-
-)
-
-# Журнал
-journal = sqlalchemy.Table(
-    "journal",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("date", sqlalchemy.Date, default=datetime.datetime.utcnow),
-    sqlalchemy.Column("violation", sqlalchemy.ForeignKey('violation.id'), nullable=False),
-    sqlalchemy.Column("measures", sqlalchemy.String(128)),
-
-)
-
-
+# # Операции
+# operation = sqlalchemy.Table(
+#     "operation",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("name", sqlalchemy.String(128)),
+#     sqlalchemy.Column("process_id", sqlalchemy.ForeignKey('process.id'), nullable=False),
+#
+# )
+#
+#
+# # должность работника
+# employ_position = sqlalchemy.Table(
+#     "employ_position",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("name", sqlalchemy.String(128)),
+#
+# )
+#
+# # работник
+# worker = sqlalchemy.Table(
+#     "worker",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("name", sqlalchemy.String(128)),
+#     sqlalchemy.Column("surname", sqlalchemy.String(128)),
+#     sqlalchemy.Column("employ_position", sqlalchemy.ForeignKey('employ_position.id'), nullable=False),
+# )
+#
+#
+# # конрольное действие
+# control_action = sqlalchemy.Table(
+#     "control_action",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("name", sqlalchemy.String(128)),
+#
+# )
+#
+# # метод котроля
+# method = sqlalchemy.Table(
+#     "method",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("name", sqlalchemy.String(128)),
+#
+# )
+# # Справка о нарушении
+# certificate_of_violations = sqlalchemy.Table(
+#     "certificate_of_violations",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("date", sqlalchemy.Date, default=datetime.utcnow),
+#     sqlalchemy.Column("worker", sqlalchemy.ForeignKey('worker.id'), nullable=False),
+#
+# )
+#
+# # Строка в справке о нарушениях
+# violation = sqlalchemy.Table(
+#     "violation",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("certificate_of_violations", sqlalchemy.ForeignKey('certificate_of_violations.id'), nullable=False),
+#     sqlalchemy.Column("core_reestr", sqlalchemy.ForeignKey('reestr.id'), nullable=False),
+#     sqlalchemy.Column("title", sqlalchemy.String(128)),
+#     sqlalchemy.Column("employ_position", sqlalchemy.ForeignKey('employ_position.id'), nullable=False),
+#     sqlalchemy.Column("worker", sqlalchemy.ForeignKey('worker.id'), nullable=False),
+#     sqlalchemy.Column("amount", sqlalchemy.Float),
+#
+# )
+#
+# # Журнал
+# journal = sqlalchemy.Table(
+#     "journal",
+#     metadata,
+#     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+#     sqlalchemy.Column("date", sqlalchemy.Date, default=datetime.utcnow),
+#     sqlalchemy.Column("violation", sqlalchemy.ForeignKey('violation.id'), nullable=False),
+#     sqlalchemy.Column("measures", sqlalchemy.String(128)),
+#
+# )
+#
+#
 
 # Только для SQLite
 engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
@@ -143,15 +142,13 @@ async def shutdown():
     await database.disconnect()
 
 
-class UserIn(BaseModel):
-    name: str = Field(max_length=32)
-    email: str = Field(max_length=128)
+class PrsIn(BaseModel):
+    name: str = Field(max_length=128)
 
 
-class User(BaseModel):
+class Prs(BaseModel):
     id: int
-    name: str = Field(max_length=32)
-    email: str = Field(max_length=128)
+    name: str = Field(max_length=128)
 
 
 # @app.get("/fake_users/{count}")
@@ -162,23 +159,30 @@ class User(BaseModel):
 #     return {'message': f'{count} fake users create'}
 
 
-# @app.post("/users/", response_model=User)
-# async def create_user(user: UserIn):
-#     # query = users.insert().values(name=user.name, email=user.email)
-#     query = users.insert().values(**user.dict())
+# @app.post("/Process/", response_model=Prs)
+# async def create_process(prs: PrsIn):
+#     """Добавление процесса"""
+#     query = prs.insert().values(name=prs.name, id=prs.id)
+#     # query = prs.insert().values(*prs.dict())
+#     print(query)
 #     last_record_id = await database.execute(query)
-#     return {**user.dict(), "id": last_record_id}
+#     print(last_record_id)
+#     my_dict = {**prs.dict(), "id": last_record_id}
+#     print(my_dict)
+#     # return {**proces.dict(), "id": last_record_id}
+#     return prs.dict()
+
+
+@app.get("/Process/", response_model=List[Prs])
+async def read_process():
+    """выборка всех процессов"""
+    query = prs.select()
+    return await database.fetch_all(query)
+
 #
-#
-# @app.get("/users/", response_model=List[User])
-# async def read_users():
-#     query = users.select()
-#     return await database.fetch_all(query)
-#
-#
-# @app.get("/users/{user_id}", response_model=User)
-# async def read_user(user_id: int):
-#     query = users.select().where(users.c.id == user_id)
+# @app.get("/prs/{prs_id}", response_model=prs)
+# async def read_prs(prs_id=0):
+#     query = prs.select().where(prs.c.id == prs_id)
 #     return await database.fetch_one(query)
 #
 #
